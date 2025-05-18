5c625d34afee5db361d116beea86b311a72ba9a0
Kyle Housel <kylegranthousel@gmail.com>
2025-05-18 08:26:01 -0400

    Create fetch POST request and append data to DOM

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const contentDiv = document.querySelector('#content')
+const header = document.querySelector('h1')
+
+
+const submitData = (name, email) => {
+
+  const userObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({ name, email })
+  }
+
+  return fetch("http://localhost:3000/users", userObject)
+          .then((response) => {
+            return response.json();
+          })
+          .then((object) => {
+            const p = document.createElement("p");
+            p.textContent = object.id;
+            contentDiv.appendChild(p);
+          })
+          .catch((error) => {
+            header.textContent = error.message
+          });
+}
